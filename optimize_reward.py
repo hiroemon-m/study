d2ae5f3f8f24bd50c5bcaeff6194bfc79a29bc88
Matsumoto Hirotomo <matsumoto-hirotomo@matsumotonoMacBook-Air.local>
2023-10-05 12:46:50 +0900

    visualize networkdata

--- optimize_reward.py
+++ optimize_reward.py
@@ -59,6 +59,12 @@ class Optimizer:
         print(loss)
         del loss
         self.optimizer.step()
+        
+        #グラフデータの可視化
+        #--------------
+        graph_data = self.edges[t].numpy()
+        np.save('DBLP_graph_data={}.npy'.format(str(t)), graph_data)
+        #--------------
 
     def export_param(self):
         with open("model.param.data.fast", "w") as f:
@@ -97,6 +103,6 @@ if __name__ == "__main__":
     ).to(device)
     model = Model(alpha, beta)
     optimizer = Optimizer(data.adj, data.feature, model, data_size)
-    for t in range(5):
+    for t in range(10):
         optimizer.optimize(t)
     optimizer.export_param()
